20d351650ced3a4a32f5f1f41cb7a31532b4b7cf
UltralyticsAssistant <web@ultralytics.com>
2025-08-11 06:31:39 +0000

    Auto-format by https://ultralytics.com/actions

--- predict-seg.py
+++ predict-seg.py
@@ -1,9 +1,9 @@
 from ultralytics import YOLO
 
 # 加载训练的模型
-model = YOLO('runs/segment/train/weights/best.pt')
+model = YOLO("runs/segment/train/weights/best.pt")
 
-img_list = ['cat.png']
+img_list = ["cat.png"]
 
 for img in img_list:
     # 运行推理，并附加参数 save:是否保存文件 retina_masks：返回高分辨率分割掩码
--- test.py
+++ test.py
@@ -2,8 +2,8 @@ from ultralytics import YOLO
 
 if __name__ == "__main__":
     # 加载预训练的 YOLOv11n 模型
-    model = YOLO('yolo11n.pt')
-    source = 'cat.png'
+    model = YOLO("yolo11n.pt")
+    source = "cat.png"
 
     results = model.predict(source)
 
